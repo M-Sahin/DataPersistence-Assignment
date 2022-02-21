e59bb7f748443fca284ecb633dbd2a72c8899ea9
Murat Sahin <Murat.sa25@gmail.com>
2022-02-21 14:13:51 +0100

    Add project files.

---
+++ DataPersistence-Assignment/DataAccess/ConnectionStringHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPersistence_Assignment.DataAccess
+{
+    public class ConnectionStringHelper
+    {
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            sqlConnectionStringBuilder.DataSource = "MURAT-LAPTOP\\SQLEXPRESS";
+            sqlConnectionStringBuilder.InitialCatalog = "Chinook";
+            sqlConnectionStringBuilder.IntegratedSecurity = true;
+            sqlConnectionStringBuilder.TrustServerCertificate = true;
+
+            return sqlConnectionStringBuilder.ConnectionString;
+        }
+    }
+}
---
+++ DataPersistence-Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataPersistence_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //POCO
+        }
+    }
+}
